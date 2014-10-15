f5cee360807b89c8c39fa0cc4278cac9e6d455b7
MaximeHeckel <heckelmaxime@gmail.com>
2014-10-15 10:01:53 +0200

    prepare config file

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // the database url to connect
+  url : '',
+  //Yelp API keys
+  yelpClientId : '',
+  yelpClientSecret : '',
+  //Foursquare API keys
+  fsquareClientId : '',
+  fsquareClientSecret : ''
+}
