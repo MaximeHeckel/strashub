d4fbe1e3330d571820ac7ab9bfaa5cc2b566cf92
MaximeHeckel <heckelmaxime@gmail.com>
2014-10-15 11:40:37 +0200

    venue api working

---
+++ libs/fsquare_handler.js
@@ -0,0 +1,40 @@
+var fs = require('fs'),
+       request = require('request'),
+       querystring = require('querystring')
+       OAuth = require('oauth').OAuth,
+       OAuth2 = require('oauth').OAuth2;
+
+//var fsquareConfig = JSON.parse(fs.readFileSync('./config/config.js'))
+var fsquareApiUrl = 'https://api.foursquare.com/v2'
+var fsquareUrl = 'http://foursquare.com/'
+var fsquareSuffix = '/venues/explore'
+var fsquareSearchParams = {
+  near : "Strasbourg",
+  section : "outdoors",
+  intent : "browse",
+  radius : "4000",
+  v : "20140801" //DON'T CHANGE
+}
+var fsquareAuth = {
+  client_id : 'LLO0IEM00UATGQ31N1GNZHL5F5EGPVR3WQYCHH2BAMGGIOSN' ,
+  client_secret : '0PHQLSSNCTCXLKU2LV0LITR5UG4W5UILT1DBRVXXYPHNGH5X'
+}
+var fsquareFinalUrl = fsquareApiUrl + fsquareSuffix +'?' + querystring.stringify(fsquareSearchParams) + '&' + querystring.stringify(fsquareAuth)
+var fsquareResponse = "not yet"
+
+console.log(fsquareFinalUrl);
+
+exports.strasRequest = function(callback){
+  request(fsquareFinalUrl, function(err, res, data){
+    if(err){
+      console.log("Foursquare API error: " + err);
+      fsquareResponse = err;
+      callback(err)
+    } else {
+      console.log("Success !");
+      //console.log(data);
+      fsquareResponse = data;
+      callback(null,fsquareResponse)
+    }
+  });
+}
--- routes/places.js
+++ routes/places.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fsquareHandler = require('../libs/fsquare_handler.js')
+var prettyjson = require('prettyjson')
 var router = express.Router();
 
 /* GET places listing. */
 router.get('/', function(req, res) {
-  res.send('This will send JSON');
+ fsquareHandler.strasRequest(function(err,result){
+   if(err) console.log(err);
+   result = JSON.parse(result)
+   res.send(result)
+ })
 });
 
 module.exports = router;
