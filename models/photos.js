cd33f3d217c2442df1655a33efbafba1a7b7957d
Maxime Heckel <heckelmaxime@gmail.com>
2015-01-18 12:09:48 +0100

    remove photo model

--- models/photos.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var schema = new mongoose.Schema({
-  id: String,
-  photo: {type: String, default: ""}
-});
-
-module.exports = mongoose.model('Photos', schema);
