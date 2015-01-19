bff03da890bb373d04d707b5f12b407c0a4f0d6f
Maxime Heckel <heckelmaxime@gmail.com>
2015-01-19 15:10:35 +0100

    json formating #2

--- routes/places.js
+++ routes/places.js
@@ -18,7 +18,7 @@ router.get('/tourism', function(req, res) {
   });
  Tourism.find({}, function(err,data){
    if(err) res.send(err)
-   res.json(JSON.parse(data));
+   res.json(data,200);
  })
 });
 
