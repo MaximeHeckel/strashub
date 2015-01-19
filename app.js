e5a23c2cdcf86d39a9d2583d5b5d4b91ba11cbc9
Maxime Heckel <heckelmaxime@gmail.com>
2015-01-19 15:02:26 +0100

    json formating

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('json spaces', 40);
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
